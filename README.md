ac5391f6a200775cf9be2de6fb4d0e8456c58908
Bushra Rehan <bushra.rehan@cmf.de>
2023-11-05 21:50:14 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+To install the PDF Chat App, please follow these steps:
+
+Clone the repository to your local machine.
+
+Install the required dependencies by running the following command:
+```bash
+pip install -r requirements.txt
+```
+
+Obtain an API key from OpenAI and add it to the .env file in the project directory.
+```bash
+OPENAI_API_KEY=your_secrit_api_key
+```
+
+Run the App
+
+Run the app.py file using the Streamlit CLI. Execute the following command:
+```bash
+streamlit run app.py
+```
---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://media.licdn.com/dms/image/D4E03AQFc5_G7JkY9hQ/profile-displayphoto-shrink_800_800/0/1678635044230?e=2147483647&v=beta&t=KOU11FOp-qg71tvP5sPcCyPGfUMT_7hhZ7nD0qjpcr0" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
